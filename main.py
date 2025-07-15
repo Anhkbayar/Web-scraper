5ffa42b97f69550c77c3687d141c2c5ec115d1c7
Anka <zanhaa56@gmail.com>
2025-07-15 16:14:30 +0800

    handles oemless parts

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 import time
+
+EMAIL = "ankhbayar@garage.mn"
+PASSWORD = "Ankhaa#123"
 
 #unshih
 df = pd.read_excel("TestArticle.xlsx")
@@ -22,8 +27,8 @@ time.sleep(1)
 loginbttn = driver.find_element(By.ID, "btnlogin")
 loginbttn.click()
 
-driver.find_element(By.ID, "email").send_keys("ankhbayar@garage.mn")
-driver.find_element(By.ID, "password").send_keys("Ankhaa#123", Keys.RETURN)
+driver.find_element(By.ID, "email").send_keys(EMAIL)
+driver.find_element(By.ID, "password").send_keys(PASSWORD, Keys.RETURN)
 
 #process
 for index, row in df.iterrows():
@@ -35,5 +40,18 @@ for index, row in df.iterrows():
     driver.find_element(By.ID, "articleid").send_keys(str(article_id), Keys.RETURN)
     time.sleep(1)
     
-    part_page = driver.find_element(By.CLASS_NAME, "text-body fw-bold")
-    part_page.click()
+    link = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "a.text-body.fw-bold"))
+    )
+    link.click()
+    
+    #OEM huulah
+    try:
+        h5_element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "h5.text-truncate.font-size-14"))
+        )
+
+        part_number = h5_element.text.strip()
+        print("Huulsan oem:", part_number)
+    except Exception as e:
+        print("Aldaatai articleId", str(row))
