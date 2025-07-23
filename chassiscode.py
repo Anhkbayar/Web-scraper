cafbbe7224650cd599d401e41067d6342793ada2
Anka <zanhaa56@gmail.com>
2025-07-23 18:43:46 +0800

    error catching

--- chassiscode.py
+++ chassiscode.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 import time
 from openpyxl import load_workbook
 
-FILEPATH = "split_3"
+FILEPATH = "split_4"
 
 df = pd.read_excel(FILEPATH+".xlsx", header=None)
 wb = load_workbook(FILEPATH+".xlsx")
@@ -25,45 +25,47 @@ options.add_argument('--disable-gpu')
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 
-#logic
-save_interval = 50
+try:
+    #logic
+    save_interval = 50
 
-for index, row in df.iterrows():
-    VIN = row.iloc[0]
-    print(VIN)
-    if not ws.cell(row=index + 1, column=2).value:
-        start_time = time.time()
-        driver.get("https://www.toyodiy.com/parts/q?vin="+str(VIN))
-        time.sleep(0.5)
-        try:
-            production_date = driver.find_element(By.XPATH, '//a[@title="production date"]').text.strip()
-            row_xpath = '//table[@class="res"]/tbody/tr[2]' 
-            model_code = driver.find_element(By.XPATH, row_xpath + '/td[2]').text.strip()
-            from_date = driver.find_element(By.XPATH, row_xpath + '/td[3]').text.strip()
-            to_date = driver.find_element(By.XPATH, row_xpath + '/td[4]').text.strip()
+    for index, row in df.iterrows():
+        VIN = row.iloc[0]
+        print(VIN)
+        if not ws.cell(row=index + 1, column=2).value:
+            start_time = time.time()
+            driver.get("https://www.toyodiy.com/parts/q?vin="+str(VIN))
+            time.sleep(0.3)
+            try:
+                production_date = driver.find_element(By.XPATH, '//a[@title="production date"]').text.strip()
+                row_xpath = '//table[@class="res"]/tbody/tr[2]' 
+                model_code = driver.find_element(By.XPATH, row_xpath + '/td[2]').text.strip()
+                from_date = driver.find_element(By.XPATH, row_xpath + '/td[3]').text.strip()
+                to_date = driver.find_element(By.XPATH, row_xpath + '/td[4]').text.strip()
 
-            print("Model Code:", model_code)
-            print("From Date:", from_date)
-            print("To Date:", to_date)
+                print("Model Code:", model_code)
+                print("From Date:", from_date)
+                print("To Date:", to_date)
 
-            ws.cell(row=index + 1, column=2, value=model_code)  
-            ws.cell(row=index + 1, column=3, value=from_date)   
-            ws.cell(row=index + 1, column=4, value=to_date) 
-            ws.cell(row=index + 1, column=5, value=production_date)    
-        except Exception as e:
-            print("Aldaa")
-            ws.cell(row = index + 1, column = 2, value = "Not found")
-            
-        if index % save_interval == 0:
-            wb.save(FILEPATH+".xlsx")
-            
-        end_time = time.time()
-        hugatsaa = end_time - start_time
-        print(f"hugatsaa {hugatsaa:.3f}")
-        print(f"Row: {index}")
-    else:
-        print(f"Row {index} processed")
-        continue
-    
-driver.quit()
-print("Amjilttai")
+                ws.cell(row=index + 1, column=2, value=model_code)  
+                ws.cell(row=index + 1, column=3, value=from_date)   
+                ws.cell(row=index + 1, column=4, value=to_date) 
+                ws.cell(row=index + 1, column=5, value=production_date)    
+            except Exception as e:
+                print("Aldaa")
+                ws.cell(row = index + 1, column = 2, value = "Not found")
+                
+            if index % save_interval == 0:
+                wb.save(FILEPATH+".xlsx")
+                
+            end_time = time.time()
+            hugatsaa = end_time - start_time
+            print(f"hugatsaa {hugatsaa:.3f}")
+            print(f"Row: {index}")
+        else:
+            print(f"Row {index} processed")
+            continue
+finally:        
+    wb.save(FILEPATH+".xlsx")
+    driver.quit()
+    print("Amjilttai")
